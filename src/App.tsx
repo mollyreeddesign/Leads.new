5e88bdbe4e3d9f0f5fec37ef751bd9e6d2cf6919
valeriejuradoweb <valeriejuradoweb@gmail.com>
2025-11-28 11:45:32 +0100

    feat: Remove preview page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import { useState } from 'react'
 import EditLeadMagnetPanel from './components/EditLeadMagnetPanel'
-import LeadMagnetPreview from './components/LeadMagnetPreview'
 
 function App() {
-  const [currentPage, setCurrentPage] = useState<'edit' | 'preview'>('preview')
-
   return (
     <div className="relative">
-      {currentPage === 'preview' ? (
-        <LeadMagnetPreview onEditClick={() => setCurrentPage('edit')} />
-      ) : (
-        <EditLeadMagnetPanel onPreviewClick={() => setCurrentPage('preview')} />
-      )}
+      <EditLeadMagnetPanel onPreviewClick={() => {}} />
     </div>
   )
 }
